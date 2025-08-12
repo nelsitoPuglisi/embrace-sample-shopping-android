e3369e484a06a8db1b31cd01e3136670c39119fc
Nelsito <nelson.puglisi@protonmail.com>
2025-08-12 14:53:18 -0300

    Apply edge-to-edge insets to main screen

--- app/src/main/java/io/embrace/shoppingcart/MainActivity.kt
+++ app/src/main/java/io/embrace/shoppingcart/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,7 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EmbraceShoppingCartTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> MainActionsScreen() }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainActionsScreen(modifier = Modifier.padding(innerPadding))
+                }
             }
         }
     }
